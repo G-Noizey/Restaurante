ff8a44e5bae49349b21fc1acc86537cc7e354e60
Moses <moymorcol98@gmail.com>
2024-03-13 08:56:14 -0600

    al2

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import {} from "react";
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
 import Navigation from "./src/config/navigation/Navigation";
 import { app, auth, db, storage } from "./src/config/util/firebaseConnection";
+import Notification from './src/modules/notifications/Notification';
 
 export default function App() {
-  return <Navigation />;
+  return <Notification />;
 }
